cc80675113bad50829a92c6f38a753581aac152f
Alabalala <allareiksa@gmail.com>
2024-08-19 18:21:49 +0200

    changed hamburguer menu and fixed footer

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ function showDropDown() {
   const dropDown = document.querySelector(".dropDown");
   if (dropDown.hasAttribute("hidden")) {
     dropDown.removeAttribute("hidden");
+    menuIcon.classList.remove("fa-solid");
+    menuIcon.classList.remove("fa-bars");
+    menuIcon.classList.add("fa-solid");
+    menuIcon.classList.add("fa-x");
     document.addEventListener("click", (event) => {
       if (
         !dropDown.contains(event.target) &&
@@ -12,11 +16,19 @@ function showDropDown() {
         !menuIcon.contains(event.target)
       ) {
         dropDown.setAttribute("hidden", "");
+        menuIcon.classList.remove("fa-solid");
+        menuIcon.classList.remove("fa-x");
+        menuIcon.classList.add("fa-solid");
+        menuIcon.classList.add("fa-bars");
       } else {
         return;
       }
     });
   } else {
     dropDown.setAttribute("hidden", "");
+    menuIcon.classList.remove("fa-solid");
+    menuIcon.classList.remove("fa-x");
+    menuIcon.classList.add("fa-solid");
+    menuIcon.classList.add("fa-bars");
   }
 }
