fe2439fd7b45d0abc5c479c04c479f2e46eb3192
Alabalala <allareiksa@gmail.com>
2024-08-19 11:39:04 +0200

    added gif search

---
+++ cats.js
@@ -0,0 +1,33 @@
+const img = document.getElementById("gif");
+fetch(
+  "https://api.giphy.com/v1/gifs/translate?api_key=QWzzqeKYGdLJrCbWy8aT72WhcMwEYTno&s=cats",
+  {
+    mode: "cors",
+  }
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (response) {
+    img.src = response.data.images.original.url;
+  });
+
+const findButton = document.getElementById("buscar");
+const searchBox = document.querySelector(".class");
+findButton.addEventListener("click", findCat);
+
+function findCat() {
+  searchInput = findButton.value;
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=QWzzqeKYGdLJrCbWy8aT72WhcMwEYTno&s=${searchInput}+cat`,
+    {
+      mode: "cors",
+    }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
+}
